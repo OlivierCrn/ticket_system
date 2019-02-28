9b82b3e7654b9fce56c66843996bb4ee0747a134
$Name = "Olivier <crn.olivier@gmail.com>
2019-02-28 17:12:17 +0100

    Update Userpage.php
    Add comments

--- UserPage.php
+++ UserPage.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="style.css">
 </head>
 
+    // Load the script to chose between Good Morning, good afternoon or good evening 
 <body onLoad="whichHello();">
     <nav class="navbar navbar-expand-lg navbar-light " style="background-color: #c3cfe5;">
         <a class="navbar-brand mx-auto" href="#">CRNO - Ticket system - Login</a>
@@ -30,7 +31,7 @@
             </ul>
         </div>
     </nav>
-
+// Manage interne nav button
     <ul class="nav nav-pills mb-3 justify-content-center" id="pills-tab" role="tablist">
         <li class="nav-item">
             <a class="nav-link active active_button" id="pills-home-tab" data-toggle="pill" href="#pills-home"
@@ -47,15 +48,20 @@
     </ul>
     <div class="tab-content" id="pills-tabContent" style="width: 100%">
         <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+            // Manage alert (e.g: when a ticket is sent)
         <div class="alert alert-success" role="alert"> <?php echo($_SESSION['notification_message']) ?> </div>
             <div id="hello"> <?php echo $_SESSION["nickname"]; ?> welcome in the CRNO - Ticket system. You can send and
                 consult your account's ticket. </div>
         </div>
+        
+        // Include the page create_tickets 
         <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
             <?php
             include('create_tickets.php')
             ?>
         </div>
+        
+        // Include the page display_tickets 
         <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
             <?php
             include('display_tickets.php')
@@ -72,12 +78,14 @@
         </script>
         <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+        //This script build the table for user's ticket
         <script type="text/javascript">
         $(document).ready(function () 
         {
             $('#tickets').DataTable();
         } );
         </script>
+        //This script defines what kind of greetings is used
         <script type="text/javascript">
         function whichHello() {
             var d = new Date();
